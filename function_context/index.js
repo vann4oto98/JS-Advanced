9380faa0693d2828f13554c09db216b830abd59a
Vanyo Ivanov <ivanhaze@abv.bg>
2020-06-11 17:00:58 +0300

    update

--- function_context/index.js
+++ function_context/index.js
@@ -2,16 +2,21 @@ function solve() {
    let body = document.querySelector("tbody");
    let last = "";
 
-
    body.addEventListener("click", changeColor);
-
 
    function changeColor(e) {
       if (last !== "") {
          last.removeAttribute("style")
       }
-      last = e.target.parentNode;
-      last.style.backgroundColor = "#413f5e"
-   }
 
+      let current = e.target.parentNode;
+
+      if (current === last){
+         last.removeAttribute("style");
+         return;
+      }
+      
+      current.style.backgroundColor = "#413f5e";
+      last = current;
+   }
 }
