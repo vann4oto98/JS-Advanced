aaa482d27cf35e501e34576e037f60cf5c5f7475
Vanyo Ivanov <ivanhaze@abv.bg>
2020-06-14 12:46:44 +0300

    update

--- advanced_functions/personalBMI.js
+++ advanced_functions/personalBMI.js
@@ -1,49 +1,62 @@
-class Person {
-    constructor(name, age, weight, height) {
-        this.name = name;
-        this.age = age;
-        this.weight = weight;
-        this.height = height;
-        //this.bmi = Math.ceil(this.weight/(Math.pow(this.height/100)));  //връща нан
-    }
-
-    get status() {
-        if (this.bmi >= 30) {    //нарочно така съм го направил за да влиза в get bmi, връща ми числата и после нан?
-            return "obese";
-        } else if (this.bmi >= 25) {
-            return "overweight";
-        } else if (this.bmi >= 18.5) {
-            return "normal";
-        }
-    }
-
-    get bmi() {
-        console.log(this.weight);  // връща си правилно
-        console.log(this.height);  //
-        return Math.ceil(this.weight / (Math.pow(this.height / 100))); // връща нан
-    }
-
-    info() {
-        return {
-            name: this.name,
-            personalInfo: {
-                age: this.age,
-                weight: this.weight,
-                height: this.height
-            },
-            BMI: this.bmi,        // 
-            status: this.status  // 
-        }
-    }
-}
 
 function calculateBMI(name, age, weight, height) {
 
-    let person = new Person(name, age, weight, height);
+    class Person {
+        constructor(name, age, weight, height) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.height = height;
+        }
 
-    console.log(person.bmi);
+        get status() {
+            if (this.bmi >= 30) {
+                return "obese";
+            } else if (this.bmi >= 25) {
+                return "overweight";
+            } else if (this.bmi >= 18.5) {
+                return "normal";
+            } else {
+                return "underweight";
+            }
+        }
+
+        get bmi() {
+            return Math.round(this.weight / (Math.pow((this.height / 100), 2)));
+        }
+
+        info() {
+            if (this.status === "obese") {
+                return {
+                    name: this.name,
+                    personalInfo: {
+                        age: this.age,
+                        weight: this.weight,
+                        height: this.height
+                    },
+                    BMI: this.bmi,
+                    status: this.status,
+                    recommendation: "admission required"
+                }
+            } 
+
+            return {
+                name: this.name,
+                personalInfo: {
+                    age: this.age,
+                    weight: this.weight,
+                    height: this.height
+                },
+                BMI: this.bmi,
+                status: this.status
+            }
+        }
+    }
+
+
+    let person = new Person(name, age, weight, height);
 
     return person.info();
 }
 
-console.log(calculateBMI("Peter", 29, 75, 182));
+console.log(calculateBMI("Peter", 9, 57, 165));
