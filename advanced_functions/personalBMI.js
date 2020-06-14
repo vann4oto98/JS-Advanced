d11a8029eafcd22abf7e99a0c68f15282b834db9
Vanyo Ivanov <ivanhaze@abv.bg>
2020-06-14 12:24:53 +0300

    уп

--- advanced_functions/personalBMI.js
+++ advanced_functions/personalBMI.js
@@ -1,5 +1,5 @@
-class Person{
-    constructor(name, age, weight, height){
+class Person {
+    constructor(name, age, weight, height) {
         this.name = name;
         this.age = age;
         this.weight = weight;
@@ -7,38 +7,37 @@ class Person{
         //this.bmi = Math.ceil(this.weight/(Math.pow(this.height/100)));  //връща нан
     }
 
-    get status(){
-        if (this.bmi >= 30){    //нарочно така съм го направил за да влиза в get bmi, връща ми числата и после нан?
+    get status() {
+        if (this.bmi >= 30) {    //нарочно така съм го направил за да влиза в get bmi, връща ми числата и после нан?
             return "obese";
-        } else if (this.bmi >= 25){
+        } else if (this.bmi >= 25) {
             return "overweight";
-        } else if (this.bmi >= 18.5){
+        } else if (this.bmi >= 18.5) {
             return "normal";
         }
     }
 
-    get bmi(){
+    get bmi() {
         console.log(this.weight);  // връща си правилно
         console.log(this.height);  //
-        return Math.ceil(this.weight/(Math.pow(this.height/100))); // връща нан
+        return Math.ceil(this.weight / (Math.pow(this.height / 100))); // връща нан
     }
 
-    info(){
-       return {
-           name: this.name,
-           personalInfo: {
-               age: this.age,
-               weight: this.weight,
-               height: this.height
-           },
-           BMI: this.bmi,        // 
-           status: this.status  // 
-       }
+    info() {
+        return {
+            name: this.name,
+            personalInfo: {
+                age: this.age,
+                weight: this.weight,
+                height: this.height
+            },
+            BMI: this.bmi,        // 
+            status: this.status  // 
+        }
     }
 }
 
-function calculateBMI(name, age, weight, height){
-
+function calculateBMI(name, age, weight, height) {
 
     let person = new Person(name, age, weight, height);
 
