0530afa51d33f9edc254300b4d61af4afa3f6e89
Vanyo <61576293+vann4oto98@users.noreply.github.com>
2020-06-24 19:52:37 +0300

    ex

--- exam_preps/may2020/pressHouse.js
+++ exam_preps/may2020/pressHouse.js
@@ -1,108 +1,102 @@
-function pressHouse() {   //todo: continue
+function solve(){
     class Article{
-        constructor(title, content){
+        constructor(title, content) {
             this.title = title;
             this.content = content;
         }
-    
-        toString() {
-            return `Title: ${this.title}\nContent: ${this.content}`
-        }
-    }
-    
-    class ShortReports extends Article{
-        constructor(title, content, originalResearches, comments){
-            super(title, content);
-            this.originalResearches = originalResearches;
-            this.comments = comments;
-        }
-
-        set content(content){
-            if (content.length >= 150) {
-                throw new Error("Short reports content should be less then 150 symbols.");
-            }
-        }
-
-        set originalResearches(originalResearches){
-            if (this.originalResearches["title"] === undefined || this.originalResearches["author"] === undefined) {
-                throw new Error ("The original research should have author and title.")
-            }
-        }
-
-        addComment(comment){
-            this.comments.push(comment);
-            return "The comment is added."
-        }
 
         toString(){
-            let output = super.toString() + `\nOriginal Research: ${this.title} by ${this.author}`;
-            if (this.comments.length >0){
-                output += `\nComments:`
-                this.comments.forEach(element => {
-                    output += `\n${element}`;
-                });
-            } 
-            return output;
+            return[
+                `Title: ${this.title}`,
+                `Content: ${this.content}`
+            ].join('\n');
         }
     }
 
+    class ShortReports extends Article{
+        constructor(title, content, originalResearch){
+            if (content.length >= 150) {
+                throw new Error("Short reports content should be less then 150 symbols.");
+            } else if (originalResearch.hasOwnProperty('title') == false ||
+            originalResearch.hasOwnProperty("author") == false) {
+                throw new Error("The original research should have author and title.")
+            }
+            super(title, content);
+            this.originalResearches = originalResearch;
+            this.comments = [];
+        }
+        addComment(comment) {
+            this.comments.push(comment);
+            return 'The comment is added.';
+        }
 
+        toString() {
+            const result = [
+                super.toString(),
+                `Original Research: ${this.originalResearches.title} by ${this.originalResearches.author}`
+            ];
+
+            if (this.comments.length > 0){ 
+                result.push(`Comments:`);
+                this.comments.forEach(c => result.push(c));
+            }
+
+            return result.join('\n');
+        }
+    }
 
     class BookReview extends Article{
-        constructor(title, content, book, clients){
+        constructor(title, content, book){
             super(title, content);
             this.book = book;
-            this.clients = clients;
-            this.orders = [];
+            this.clients = [];
         }
 
         addClient(clientName, orderDescription){
-            this.clients.forEach(client =>{
-                if (client.clientName === clientName){
-                    if (client.orderDescription === orderDescription){
-                        throw new Error("This client has already ordered this review.");
-                    }
-                }
-            });
-            this.orders.push({clientName, orderDescription});
-            return `${clientName} has ordered a review for ${orderDescription}`;
+            if (this.clients.find(c => c.clientName == clientName) !== undefined){
+                throw new Error("This client has already ordered this review.");
+            } else {
+                this.clients.push({
+                    clientName,
+                    orderDescription
+                });
+                return `${clientName} has ordered a review for ${this.book.name}`;
+            }
         }
 
-        toString(){
-            let output = super.toString();
-            output += `\nBook: ${this.book.name}`;
-            if (this.orders.length >0){
-                output += `\nOrders:`;
-                this.orders.forEach(order =>{
-                    output += `\n${order.clientName} - ${order.orderDescription}.`
-                });
+        toString() {
+            const result = [
+                super.toString(),
+                `Book: ${this.book.name}`
+            ];
+
+            if (this.clients.length > 0){ 
+                result.push(`Orders:`);
+                this.clients.forEach(c => result.push(`${c.clientName} - ${c.orderDescription}`));
             }
-            return output;
+
+            return result.join('\n');
         }
     }
 
     return {
-        Article, 
+        Article,
         ShortReports,
         BookReview
-    };
+    }
 }
 
-let classes = pressHouse();
-let lorem = new classes.Article("Lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non tortor finibus, facilisis mauris vel…");
-console.log(lorem.toString()); 
-//------------------------------
-let short = new classes.ShortReports("SpaceX and Javascript", "Yes, its damn true.SpaceX in its recent launch Dragon 2 Flight has used a technology based on Chromium and Javascript. What are your views on this ?", { title: "Dragon 2", author: "wikipedia.org" });
-console.log(short.addComment("Thank god they didn't use java."))
-short.addComment("In the end JavaScript's features are executed in C++ — the underlying language.");
-console.log(short.toString()); 
-//------------------------------
-let book = new classes.BookReview("The Great Gatsby is so much more than a love story", "The Great Gatsby is in many ways similar to Romeo and Juliet, yet I believe that it is so much more than just a love story. It is also a reflection on the hollowness of a life of leisure. ...", { name: "The Great Gatsby", author: "F Scott Fitzgerald" });
-console.log(book.addClient("The Guardian", "100 symbols"));
-console.log(book.addClient("Goodreads", "30 symbols"));
-console.log(book.toString()); 
+let classes = solve();
+let lorem = new classes.Article("Lorem", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non tortor finibus, facilisis mauris vel…");
+console.log(lorem.toString()); 
 
+let short = new classes.ShortReports("SpaceX and Javascript", "Yes, its damn true.SpaceX in its recent launch Dragon 2 Flight has used a technology based on Chromium and Javascript. What are your views on this ?", { title: "Dragon 2", author: "wikipedia.org" });
+console.log(short.addComment("Thank god they didn't use java."))
+short.addComment("In the end JavaScript's features are executed in C++ — the underlying language.")
+console.log(short.toString()); 
 
+let book = new classes.BookReview("The Great Gatsby is so much more than a love story", "The Great Gatsby is in many ways similar to Romeo and Juliet, yet I believe that it is so much more than just a love story. It is also a reflection on the hollowness of a life of leisure. ...", { name: "The Great Gatsby", author: "F Scott Fitzgerald" });
+console.log(book.addClient("The Guardian", "100 symbols"));
+console.log(book.addClient("Goodreads", "30 symbols"));
+console.log(book.toString()); 
 
-
-   
